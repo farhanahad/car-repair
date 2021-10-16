526b28e62e1b63aaf8593cc62278c4c207e32568
Farhan <farhanahad3@gmail.com>
2021-10-16 10:22:31 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVzDciiI4AuG-Sge_D96nmdpdMgDoy74c",
+    authDomain: "car-repair-7a5c4.firebaseapp.com",
+    projectId: "car-repair-7a5c4",
+    storageBucket: "car-repair-7a5c4.appspot.com",
+    messagingSenderId: "256293951358",
+    appId: "1:256293951358:web:9de334f37cb997b85cc52f"
+  };
+  export default firebaseConfig;
